48e0908040bd4d2ce4cd264f72798c5de76711f2
AdityaNair102001 <72807512+AdityaNair102001@users.noreply.github.com>
2023-04-29 18:01:19 +0530

    feat: designed landing page

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -1,8 +1,8 @@
-import React from "react";
-
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import { Spinner, Image, Button, Center, Box, Heading } from "@chakra-ui/react";
+import Hero from "../Hero.svg";
 async function getLoginLink(setLoginlink) {
   try {
     const response = await axios.get("http://localhost:3001/auth");
@@ -18,7 +18,28 @@ function LandingPage() {
   }, []);
   return (
     <div className="App">
-      {loginLink ? <Link to={loginLink}>Login</Link> : "Loading"}
+      <Heading>
+        <span style={{ color: "#4299E1" }}>Docu</span>Sign
+      </Heading>
+      <Center
+        width={"70%"}
+        // border={"1px solid black"}
+        m={"4rem auto"}
+        flexDirection={"column"}
+      >
+        <Image width={["100%", "70%", "60%", "30%"]} src={Hero}></Image>
+        {loginLink ? (
+          <Box m={2} width={["100%", "70%", "60%", "25%"]}>
+            <Link to={loginLink}>
+              <Button width={"full"} variant={"solid"} colorScheme="blue">
+                Login
+              </Button>
+            </Link>
+          </Box>
+        ) : (
+          <Spinner></Spinner>
+        )}
+      </Center>
     </div>
   );
 }
